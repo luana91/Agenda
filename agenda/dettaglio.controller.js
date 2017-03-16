ac801a9b92d31fb3c7d8e4e36efcaa286e0f3c79
LUNA <lulypoggio@gmail.com>
2017-03-16 10:16:31 +0100

    loading e background color

--- agenda/dettaglio.controller.js
+++ agenda/dettaglio.controller.js
@@ -1,4 +1,10 @@
-angular.module('app').controller('DettaglioCtrl', function ($scope, AgendaSrv, $state) {
+angular.module('app').controller('DettaglioCtrl', function ($scope, AgendaSrv, $state, $timeout) {
 
-    $scope.appuntamento = AgendaSrv.getAppuntamento($state.params.id);
+    $scope.loading = true;
+
+    $timeout(function () {
+        $scope.appuntamento = AgendaSrv.getAppuntamento($state.params.id);
+        $scope.loading = false;
+    }, 500);
+
 });
